c2882022ff97473534df461749b0005afd5b9d73
Jin hyeon <kangjin627@gmail.com>
2023-10-13 09:20:25 +0900

    💄:: 컬러 추가

---
+++ src/styles/colors.ts
@@ -0,0 +1,21 @@
+export const color = {
+    Red: "#EE3131",
+    Gray: {
+        scale_100: "#FFFFFF",
+        scale_200: "#FAFAFC",
+        scale_300: "#EEEEF5",
+        scale_400: "#CECED1",
+        scale_500: "#8E8E9D",
+        scale_600: "#575767",
+        scale_700: "#2A2A34",
+        scale_800: "#121317",
+    },
+    Main: {
+        primary_100: "#EFF9F8",
+        primary_200: "#E0F4F1",
+        primary_300: "#1EB7A4",
+        primary_400: "#1DA796",
+    },
+    Black: "#000000",
+    White: "#FFFFFF",
+}
